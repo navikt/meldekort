d0b4ce69556b2973d8769a514949b0e5ef0318cb
My Thao Nguyen <you@example.com>
2019-02-08 09:02:25 +0100

    mellomlagring

---
+++ src/app/components/tabell/tabell.tsx
@@ -0,0 +1,14 @@
+/*import * as React from 'react';
+
+interface tabellProps {
+
+}
+
+const Tabell: React.StatelessComponent<tabellProps> = (props) => {
+
+    return (
+
+    );
+};
+
+export default Tabell;*/
--- src/app/pages/sendMeldekort/sendMeldekort.tsx
+++ src/app/pages/sendMeldekort/sendMeldekort.tsx
@@ -4,24 +4,34 @@ import Sprakvelger from '../../components/sprakvelger/sprakvelger';
 import AlertStripe from 'nav-frontend-alertstriper';
 import { FormattedMessage, FormattedHTMLMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import KnappBase from 'nav-frontend-knapper';
 
 class SendMeldekort extends React.Component<any, any> {
     constructor(props: any) {
         super(props);
     }
 
+    // bruker sitt antall meldekort må hentes fra store (etter at vi har fått koblet sammen back2front)
+
     render() {
         return(
-            <div className="sideinnhold">
-                <Innholdstittel> Send Meldekort siden </Innholdstittel>
+            <main className="sideinnhold">
+                <Innholdstittel className="seksjon"> [X] meldekort klar for innsending </Innholdstittel>
                 <Sprakvelger/>
-                <FormattedMessage id="annetFravaer.Required_AAP" defaultMessage="Hallois!" />
-                <AlertStripe type="info" solid={true}>
-                    <FormattedHTMLMessage id="sendMeldekort.alert.forklaring"/>
-                </AlertStripe>
-
-
-            </div>
+                <section className="seksjon">
+                    <FormattedMessage id="annetFravar.required-AAP" defaultMessage="Hallois!" />
+                </section>
+                <section className="seksjon">
+                    <AlertStripe type="info" solid={true}>
+                        <FormattedHTMLMessage id="sendMeldekort.alert.forklaring"/>
+                    </AlertStripe>
+                </section>
+                <section className="seksjon flex-innhold sentrert">
+                    <KnappBase type="hoved">
+                        <FormattedMessage id="sendMeldekort.knapp.startUtfylling" />
+                    </KnappBase>
+                </section>
+            </main>
         );
     }
 }
