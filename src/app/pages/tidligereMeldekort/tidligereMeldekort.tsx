608e9c9e15c5ebb29e2e60b229dbe3762adf0632
My Thao Nguyen <you@example.com>
2019-02-11 16:26:45 +0100

    har lagt inn tabellen, men trenger litt tweaking

--- src/app/pages/tidligereMeldekort/tidligereMeldekort.tsx
+++ src/app/pages/tidligereMeldekort/tidligereMeldekort.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { Innholdstittel } from 'nav-frontend-typografi';
+import { FormattedMessage } from 'react-intl';
 import Sprakvelger from '../../components/sprakvelger/sprakvelger';
+import Tabell from '../../components/tabell/tabell';
 
 class TidligereMeldekort extends React.Component<any, any> {
     constructor(props: any) {
@@ -8,10 +10,36 @@ class TidligereMeldekort extends React.Component<any, any> {
     }
 
     render() {
+
+        const rows = [
+            {'periode': 'uke 31-32', 'dato': '30. jul 2018 - 13. aug 2018'},
+            {'periode': 'uke 29-30', 'dato': '16. jul 2018 - 30. jul 2018'},
+            {'periode': 'uke 27-28', 'dato': '2. jul 2018 - 16. jul 2018'},
+        ];
+        const columns = [
+            {key: 'periode', label: 'Periode'},
+            {key: 'dato', label: 'Dato'}
+        ];
+
         return(
             <div className="sideinnhold">
-                <Innholdstittel> Tidligere meldekort</Innholdstittel>
-                <Sprakvelger/>
+                <Innholdstittel className="seksjon"> Tidligere meldekort </Innholdstittel>
+                <section className="seksjon">
+                    <Sprakvelger/>
+                </section>
+                <section className="seksjon">
+                    <FormattedMessage id="tidligereMeldekort.forklaring" />
+                </section>
+                <section className="seksjon">
+                    <FormattedMessage id="tidligereMeldekort.forklaring.korrigering" />
+                </section>
+                <section className="seksjon">
+                    <Tabell
+                        rows={rows}
+                        columns={columns}
+                        
+                    />
+                </section>
             </div>
         );
     }
