694588bff24e9bd1a10cbf40766c036f6feb54e9
My Thao Nguyen <you@example.com>
2019-02-11 12:38:05 +0100

    mellomlagring

--- src/app/components/tabell/tabell.tsx
+++ src/app/components/tabell/tabell.tsx
@@ -1,14 +1,22 @@
-/*import * as React from 'react';
+import * as React from 'react';
 
-interface tabellProps {
-
+interface TabellProps {
+    rows: {}[];
+    columns: {}[];
+    className?: string;
 }
 
-const Tabell: React.StatelessComponent<tabellProps> = (props) => {
+const Tabell: React.StatelessComponent<TabellProps> = (props) => {
+    // const rows = props.rows.map( r => (<Normaltekst> {r} </Normaltekst>));
+    const JsonTable = require('ts-react-json-table');
 
     return (
-
+        <JsonTable
+            rows={props.rows}
+            columns={props.columns}
+            className={props.className}
+        />
     );
 };
 
-export default Tabell;*/
+export default Tabell;
--- src/app/pages/sendMeldekort/sendMeldekort.tsx
+++ src/app/pages/sendMeldekort/sendMeldekort.tsx
@@ -5,6 +5,7 @@ import AlertStripe from 'nav-frontend-alertstriper';
 import { FormattedMessage, FormattedHTMLMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import KnappBase from 'nav-frontend-knapper';
+import Tabell from '../../components/tabell/tabell';
 
 class SendMeldekort extends React.Component<any, any> {
     constructor(props: any) {
@@ -14,12 +15,28 @@ class SendMeldekort extends React.Component<any, any> {
     // bruker sitt antall meldekort må hentes fra store (etter at vi har fått koblet sammen back2front)
 
     render() {
+
+        const rows = [
+            {'periode': 'uke 31-32', 'dato': '30. jul 2018 - 13. aug 2018'},
+            {'periode': 'uke 29-30', 'dato': '16. jul 2018 - 30. jul 2018'},
+            {'periode': 'uke 27-28', 'dato': '2. jul 2018 - 16. jul 2018'},
+        ];
+        const columns = [
+            {key: 'periode', label: 'Periode'},
+            {key: 'dato', label: 'Dato'}
+        ];
+
         return(
             <main className="sideinnhold">
                 <Innholdstittel className="seksjon"> [X] meldekort klar for innsending </Innholdstittel>
-                <Sprakvelger/>
                 <section className="seksjon">
-                    <FormattedMessage id="annetFravar.required-AAP" defaultMessage="Hallois!" />
+                    <Sprakvelger/>
+                </section>
+                <section className="seksjon">
+                    <Tabell
+                        rows={rows}
+                        columns={columns}
+                    />
                 </section>
                 <section className="seksjon">
                     <AlertStripe type="info" solid={true}>
