68bdcea17e49d6162838578c41a7ccde11287588
Fraschetti, Yrjan <Yrjan.Fraschetti@nav.no>
2019-03-05 15:03:52 +0100

    Legger til veilederpanel i modal

--- src/app/components/modal/loggInnContent.tsx
+++ src/app/components/modal/loggInnContent.tsx
@@ -1,13 +1,17 @@
 import { Normaltekst, Undertittel } from 'nav-frontend-typografi';
 import * as React from 'react';
+import Veilederpanel from 'nav-frontend-veilederpanel';
+import veilederAdvarsel from '../ikoner/veilder_advarsel.svg';
 
 export const loggInnContent = () => {
     return (
         <div>
-            <Undertittel children={'Du må logge inn'}/>
-            <Normaltekst>
-                Du er ikke logget inn. Vi viderefører deg til innloggingsiden.
-            </Normaltekst>
+            <Veilederpanel fargetema="advarsel" svg={<img src={veilederAdvarsel}/>}>
+                <Undertittel children={'Du må logge inn'}/>
+                <Normaltekst>
+                    Du er ikke logget inn. Vi viderefører deg til innloggingsiden.
+                </Normaltekst>
+            </Veilederpanel>
         </div>
     );
 };
