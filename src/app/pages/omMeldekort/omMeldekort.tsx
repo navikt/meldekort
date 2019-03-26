0573d7a4e1c9f8ab323c85e395034a7ac79fb86b
Fraschetti, Yrjan <Yrjan.Fraschetti@nav.no>
2019-03-26 14:07:15 +0100

    Fikset lenke i om gemeldekort

--- src/app/pages/omMeldekort/omMeldekort.tsx
+++ src/app/pages/omMeldekort/omMeldekort.tsx
@@ -5,6 +5,7 @@ import { FormattedHTMLMessage, FormattedMessage } from 'react-intl';
 import Veilederpanel from 'nav-frontend-veilederpanel';
 
 import veileder from '../../ikoner/veileder.svg';
+import { hentIntl } from '../../utils/intlUtil';
 
 class OmMeldekort extends React.Component<any, any> {
     constructor(props: any) {
@@ -12,23 +13,29 @@ class OmMeldekort extends React.Component<any, any> {
     }
 
     render() {
+        console.log(hentIntl().formatMessage({id: 'genereltOmMeldekort.informasjonOmMeldekortLink'}));
         return(
             <div className="sideinnhold">
                 <section className="seksjon flex-innhold tittel-sprakvelger">
                     <Innholdstittel><FormattedMessage id="overskrift.genereltOmMeldekort" /></Innholdstittel>
                     <Sprakvelger/>
                 </section>
-                <Veilederpanel type={'plakat'} kompakt={true} svg={<img src={veileder}/>}>
+                <Veilederpanel type={'plakat'} kompakt={true} svg={<img alt={'Veileder'} src={veileder}/>}>
                     <section className="seksjon">
                         <Normaltekst><FormattedMessage id="genereltOmMeldekort.velkommen" /></Normaltekst>
                         <Normaltekst><FormattedMessage id="genereltOmMeldekort.velge" /></Normaltekst>
                         <ul>
                             <li><FormattedMessage id="genereltOmMeldekort.valg.sende" /></li>
                             <li><FormattedMessage id="genereltOmMeldekort.valg.tidligere" /></li>
-                            <li><FormattedMessage id="genereltOmMeldekort.valg.endre" /></li>
+                            <li><FormattedMessage id="genereltOmMeldekort.valg.endre"/></li>
                         </ul>
-                        {/* TODO: Fikse lenke i teksten genereltOmMeldekort.om.meldekort */}
-                        <Normaltekst><FormattedHTMLMessage id="genereltOmMeldekort.om.meldekort" /></Normaltekst>
+                        <Normaltekst><FormattedHTMLMessage
+                            id="genereltOmMeldekort.om.meldekort"
+                            values={{
+                                0: 'https://www.nav.no',
+                                1: hentIntl().formatMessage({id: 'genereltOmMeldekort.informasjonOmMeldekortLink'}).trim()
+                            }}
+                        /></Normaltekst>
                         <Normaltekst><FormattedMessage id="genereltOmMeldekort.oss" /></Normaltekst>
                     </section>
                 </Veilederpanel>
