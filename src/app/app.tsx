50dbebe03f7747ec4c2d931b7b975663564513a3
My Thao Nguyen <you@example.com>
2019-05-06 14:14:39 +0200

    gjorde om svg til egne komponenter

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,30 +1,30 @@
-import * as React from "react";
-import { Route, Switch } from "react-router";
-import { ConnectedRouter } from "connected-react-router";
-import { history, RootState } from "./store/configureStore";
-import Header from "./components/header/header";
-import MeldekortRoutes from "./sider/meldekortRoutes";
-import setupMock from "./mock/setup-mock";
-import { Dispatch } from "redux";
-import { erMock } from "./mock/utils";
-import { MeldeForm, Person } from "./types/person";
-import { PersonStatusActions } from "./actions/personStatus";
-import { PersonStatusState } from "./reducers/personStatusReducer";
-import { connect } from "react-redux";
-import Feilside from "./components/feilside/feilside";
-import UIModalWrapper from "./components/modal/UIModalWrapper";
-import { BaksystemFeilmelding } from "./types/ui";
-import { selectFeilmelding } from "./selectors/ui";
-import UIAlertstripeWrapper from "./components/feil/UIAlertstripeWrapper";
-import { MenyState } from "./types/meny";
-import { MenyPunkt } from "./utils/menyConfig";
-import { MenyActions } from "./actions/meny";
-import { Router } from "./types/router";
-import { selectRouter } from "./selectors/router";
-import { hentIntl } from "./utils/intlUtil";
-import classNames from "classnames";
-import { PersonActions } from "./actions/person";
-import { erBrukerRegistrertIArena } from "./utils/meldekortUtils";
+import * as React from 'react';
+import { Route, Switch } from 'react-router';
+import { ConnectedRouter } from 'connected-react-router';
+import { history, RootState } from './store/configureStore';
+import Header from './components/header/header';
+import MeldekortRoutes from './sider/meldekortRoutes';
+import setupMock from './mock/setup-mock';
+import { Dispatch } from 'redux';
+import { erMock } from './mock/utils';
+import { Person } from './types/person';
+import { PersonStatusActions } from './actions/personStatus';
+import { PersonStatusState } from './reducers/personStatusReducer';
+import { connect } from 'react-redux';
+import Feilside from './components/feilside/feilside';
+import UIModalWrapper from './components/modal/UIModalWrapper';
+import { BaksystemFeilmelding } from './types/ui';
+import { selectFeilmelding } from './selectors/ui';
+import UIAlertstripeWrapper from './components/feil/UIAlertstripeWrapper';
+import { MenyState } from './types/meny';
+import { MenyPunkt } from './utils/menyConfig';
+import { MenyActions } from './actions/meny';
+import { Router } from './types/router';
+import { selectRouter } from './selectors/router';
+import { hentIntl } from './utils/intlUtil';
+import classNames from 'classnames';
+import { PersonActions } from './actions/person';
+import { erBrukerRegistrertIArena } from './utils/meldekortUtils';
 
 if (erMock()) {
   setupMock();
@@ -60,10 +60,10 @@ class App extends React.Component<Props, AppState> {
   }
 
   settInnhold = () => {
-    if (this.props.personStatus.personStatus.id === "") {
+    if (this.props.personStatus.personStatus.id === '') {
       // TODO: Denne testen burde kanskje endres. Må se an hvordan vi gjør det med feilhåndtering.
       return (
-        <div className="main-container">
+        <div className={'main-container'}>
           {this.props.baksystemFeilmelding.visFeilmelding ? (
             <UIAlertstripeWrapper />
           ) : (
@@ -83,7 +83,7 @@ class App extends React.Component<Props, AppState> {
       return (
         <div>
           <Header
-            tittel={hentIntl().formatMessage({ id: "overskrift.meldekort" })}
+            tittel={hentIntl().formatMessage({ id: 'overskrift.meldekort' })}
           />
           <div
             className={classNames({ overlay: this.props.meny.erApen })}
@@ -92,10 +92,10 @@ class App extends React.Component<Props, AppState> {
               this.props.toggleMeny(!this.props.meny.erApen)
             }
           >
-            <div className={"main-container"}>
+            <div className={'main-container'}>
               <ConnectedRouter history={history}>
                 <Switch>
-                  <Route path="/" component={MeldekortRoutes} />
+                  <Route path={'/'} component={MeldekortRoutes} />
                 </Switch>
               </ConnectedRouter>
             </div>
@@ -104,21 +104,21 @@ class App extends React.Component<Props, AppState> {
       );
     } else {
       return (
-        <div className="main-container">
+        <div className={'main-container'}>
           <Feilside />
         </div>
       );
     }
-  };
+  }
 
   settAktivMenuPunktBasertPaUrl = (meny: MenyState, url: string): void => {
-    const urlparam = "/" + url.split("/")[1];
+    const urlparam = '/' + url.split('/')[1];
     for (let i = 0; i < meny.alleMenyPunkter.length; i++) {
       if (meny.alleMenyPunkter[i].urlparam === urlparam) {
         const menypunkt = meny.alleMenyPunkter[i];
       }
     }
-  };
+  }
 
   componentDidMount() {
     const { hentPersonStatus, meny, router } = this.props;
--- src/app/components/sprakvelger/sprakvelger.tsx
+++ src/app/components/sprakvelger/sprakvelger.tsx
@@ -4,54 +4,70 @@ import { RootState } from '../../store/configureStore';
 import { Select } from 'nav-frontend-skjema';
 import { IntlAction, updateIntl } from 'react-intl-redux';
 import { connect } from 'react-redux';
+import EngelskFlaggSVG from './EngelskFlaggSVG';
+import NorskFlaggSVG from './NorskFlaggSVG';
 
 const mapStateToProps = ({ intl, locales }: RootState) => {
-    return {
-        locale: intl.locale,
-        messages: intl.messages,
-        locs: locales
-    };
+  return {
+    locale: intl.locale,
+    messages: intl.messages,
+    locs: locales
+  };
 };
 
 const mapDispatchToProps = (dispatch: Dispatch<IntlAction>) => {
-    return {
-        updateIntl: (locale: any, messages: any) =>
-            dispatch(updateIntl({locale, messages}))
-    };
+  return {
+    updateIntl: (locale: any, messages: any) =>
+      dispatch(updateIntl({ locale, messages }))
+  };
 };
 
-type ReduxType =
-    ReturnType<typeof mapStateToProps> &
-    ReturnType<typeof mapDispatchToProps>;
+type ReduxType = ReturnType<typeof mapStateToProps> &
+  ReturnType<typeof mapDispatchToProps>;
 
-class Sprakvelger extends React.Component<ReduxType> {
+/*const renderMenuItem = (code: ) => {
+  return (
+    <li key={code}>
+      <MenuItem className="languageToggle__menu__item">
+        <div className="languageToggle__button__flag">
+          {code === 'en' ? <UKFlagSVG /> : <NorwayFlagSVG />}
+        </div>
+        <div id={`languageCode_${code}`} className="languageToggle__button__language">
+          {getLanguageTextFromCode(code, intl)}
+        </div>
+      </MenuItem>
+    </li>
+  );
+};*/
 
-   handleOnChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        this.props.updateIntl(
-           event.target.value, this.props.locs[event.target.value].tekster
-           );
-    }
+const Sprakvelger: React.FunctionComponent<ReduxType> = (props) => {
 
-    render() {
-        const { locale, locs } = this.props;
-        const locsArray = [locs.nb, locs.en];
+  const handleOnChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    props.updateIntl(
+      event.target.value,
+      props.locs[event.target.value].tekster
+    );
+  };
 
-        return (
-            <div className="sprakvelger-container">
-                <Select
-                    label=""
-                    value={locale}
-                    onChange={this.handleOnChange}
-                >
-                    {locsArray.map( loc => {
-                        return (
-                            <option key={loc.label} value={loc.label}> {loc.tittel} </option>
-                        );
-                    })}
-                </Select>
-            </div>
-        );
-    }
-}
+  const { locale, locs } = props;
+  const locsArray = [locs.nb, locs.en];
 
-export default connect(mapStateToProps, mapDispatchToProps)(Sprakvelger);
+  return (
+    <div className="sprakvelger-container">
+      <Select label="" value={locale} onChange={handleOnChange}>
+        {locsArray.map(loc => {
+          return (
+            <option key={loc.label} value={loc.label}>
+              {loc.tittel}
+            </option>
+          );
+        })}
+      </Select>
+    </div>
+  );
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Sprakvelger);
