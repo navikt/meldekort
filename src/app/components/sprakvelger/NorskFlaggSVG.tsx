f142842c3628f45eb062f44e731f321bd9c547ac
My Thao Nguyen <you@example.com>
2019-05-07 08:51:59 +0200

    endret svg til foreldrepenger svg = For å fikse størrelsesfeil i firefox

--- src/app/components/sprakvelger/NorskFlaggSVG.tsx
+++ src/app/components/sprakvelger/NorskFlaggSVG.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
 
 const NorskFlaggSVG = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1100 800">
-    <rect width="1100" height="800" fill="#ef2b2d"/>
-    <rect width="200" height="800" x="300" fill="#fff"/>
-    <rect width="1100" height="200" y="300" fill="#fff"/>
-    <rect width="100" height="800" x="350" fill="#002868"/>
-    <rect width="1100" height="100" y="350" fill="#002868"/>
+  <svg width={25} height={18}>
+    <title>{'Flag_of_Norway'}</title>
+    <g fill="none" fillRule="evenodd">
+      <path fill="#EF2B2D" d="M0 0h25v18H0z" />
+      <path fill="#FFF" d="M6.875 0h4.688v18H6.875z" />
+      <path fill="#FFF" d="M0 6.9h25v4.5H0z" />
+      <path fill="#002868" d="M8.125 0h2.188v18H8.125z" />
+      <path fill="#002868" d="M0 7.8h25v2.4H0z" />
+    </g>
   </svg>
 );
 
