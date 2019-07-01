8c16ca23c1f2ad473fa65b64a4a178950b193fac
My Thao Nguyen <mythng94@gmail.com>
2019-07-01 15:39:46 +0200

    FIX: prettified & removed unused imports

--- src/app/components/meldekortdetaljer/begrunnelsevisning/begrunnelse.tsx
+++ src/app/components/meldekortdetaljer/begrunnelsevisning/begrunnelse.tsx
@@ -1,6 +1,4 @@
 import * as React from 'react';
-import { RootState } from '../../../store/configureStore';
-import { connect } from 'react-redux';
 import UtvidetInformasjon from '../../utvidetinformasjon/utvidetInformasjon';
 import Undertittel from 'nav-frontend-typografi/lib/undertittel';
 import { FormattedHTMLMessage, FormattedMessage } from 'react-intl';
--- src/app/sider/omMeldekort/omMeldekort.tsx
+++ src/app/sider/omMeldekort/omMeldekort.tsx
@@ -13,78 +13,96 @@ import { MenyActions } from '../../actions/meny';
 import { MenyPunkt } from '../../utils/menyConfig';
 import { MenyState } from '../../types/meny';
 import { Router } from '../../types/router';
-import { PersonStatusState } from '../../reducers/personStatusReducer';
-import { BaksystemFeilmelding } from '../../types/ui';
-import { Person } from '../../types/person';
 import { RootState } from '../../store/configureStore';
 import { selectRouter } from '../../selectors/router';
-import { selectFeilmelding } from '../../selectors/ui';
 
 interface MapStateToProps {
-    router: Router;
-    meny: MenyState;
+  router: Router;
+  meny: MenyState;
 }
 
 interface MapDispatchToProps {
-    resetInnsending: () => void;
-    settValgtMenyPunkt: (menypunkt: MenyPunkt) => void;
+  resetInnsending: () => void;
+  settValgtMenyPunkt: (menypunkt: MenyPunkt) => void;
 }
 
-class OmMeldekort extends React.Component<MapDispatchToProps&MapStateToProps, any> {
-
-    componentDidMount() {
-        const { resetInnsending, meny, settValgtMenyPunkt} = this.props;
-        resetInnsending();
-        const valgtMenyPunkt = meny.alleMenyPunkter.find(mp => mp.urlparam === window.location.pathname.slice(10));
-        if (typeof valgtMenyPunkt !== 'undefined') {
-            settValgtMenyPunkt(valgtMenyPunkt);
-        }
+class OmMeldekort extends React.Component<MapDispatchToProps & MapStateToProps, any> {
+  componentDidMount() {
+    const { resetInnsending, meny, settValgtMenyPunkt } = this.props;
+    resetInnsending();
+    const valgtMenyPunkt = meny.alleMenyPunkter.find(
+      mp => mp.urlparam === window.location.pathname.slice(10)
+    );
+    if (typeof valgtMenyPunkt !== 'undefined') {
+      settValgtMenyPunkt(valgtMenyPunkt);
     }
+  }
 
-    render() {
-        return(
-            <div className="sideinnhold">
-                <section className="seksjon flex-innhold tittel-sprakvelger">
-                    <Innholdstittel><FormattedMessage id="overskrift.genereltOmMeldekort" /></Innholdstittel>
-                    <Sprakvelger/>
-                </section>
-                <Veilederpanel type={'plakat'} kompakt={true} svg={<img alt={'Veileder'} src={veileder}/>}>
-                    <section className="seksjon">
-                        <Normaltekst><FormattedMessage id="genereltOmMeldekort.velkommen" /></Normaltekst>
-                        <Normaltekst><FormattedMessage id="genereltOmMeldekort.velge" /></Normaltekst>
-                        <ul>
-                            <li><FormattedMessage id="genereltOmMeldekort.valg.sende" /></li>
-                            <li><FormattedMessage id="genereltOmMeldekort.valg.tidligere" /></li>
-                            <li><FormattedMessage id="genereltOmMeldekort.valg.endre"/></li>
-                        </ul>
-                        <Normaltekst><FormattedHTMLMessage
-                            id="genereltOmMeldekort.om.meldekort"
-                            values={{
-                                0: 'https://www.nav.no',
-                                1: hentIntl().formatMessage({id: 'genereltOmMeldekort.informasjonOmMeldekortLink'}).trim()
-                            }}
-                        /></Normaltekst>
-                        <Normaltekst><FormattedMessage id="genereltOmMeldekort.oss" /></Normaltekst>
-                    </section>
-                </Veilederpanel>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="sideinnhold">
+        <section className="seksjon flex-innhold tittel-sprakvelger">
+          <Innholdstittel>
+            <FormattedMessage id="overskrift.genereltOmMeldekort" />
+          </Innholdstittel>
+          <Sprakvelger />
+        </section>
+        <Veilederpanel type={'plakat'} kompakt={true} svg={<img alt={'Veileder'} src={veileder} />}>
+          <section className="seksjon">
+            <Normaltekst>
+              <FormattedMessage id="genereltOmMeldekort.velkommen" />
+            </Normaltekst>
+            <Normaltekst>
+              <FormattedMessage id="genereltOmMeldekort.velge" />
+            </Normaltekst>
+            <ul>
+              <li>
+                <FormattedMessage id="genereltOmMeldekort.valg.sende" />
+              </li>
+              <li>
+                <FormattedMessage id="genereltOmMeldekort.valg.tidligere" />
+              </li>
+              <li>
+                <FormattedMessage id="genereltOmMeldekort.valg.endre" />
+              </li>
+            </ul>
+            <Normaltekst>
+              <FormattedHTMLMessage
+                id="genereltOmMeldekort.om.meldekort"
+                values={{
+                  0: 'https://www.nav.no',
+                  1: hentIntl()
+                    .formatMessage({ id: 'genereltOmMeldekort.informasjonOmMeldekortLink' })
+                    .trim(),
+                }}
+              />
+            </Normaltekst>
+            <Normaltekst>
+              <FormattedMessage id="genereltOmMeldekort.oss" />
+            </Normaltekst>
+          </section>
+        </Veilederpanel>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state: RootState): MapStateToProps => {
-    return {
-        router: selectRouter(state),
-        meny: state.meny
-    };
+  return {
+    router: selectRouter(state),
+    meny: state.meny,
+  };
 };
 
 const mapDispatchToProps = (dispatch: Dispatch): MapDispatchToProps => {
-    return {
-        resetInnsending: () => dispatch(InnsendingActions.resetInnsending()),
-        settValgtMenyPunkt: (menypunkt: MenyPunkt) =>
-            dispatch(MenyActions.settValgtMenyPunkt(menypunkt)),
-    };
+  return {
+    resetInnsending: () => dispatch(InnsendingActions.resetInnsending()),
+    settValgtMenyPunkt: (menypunkt: MenyPunkt) =>
+      dispatch(MenyActions.settValgtMenyPunkt(menypunkt)),
+  };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(OmMeldekort);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(OmMeldekort);
