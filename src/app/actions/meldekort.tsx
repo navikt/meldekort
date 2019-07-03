540b489782f3d37d28fcf352c85b0c3969e12be9
Yrjan Fraschetti <yrjanaff@gmail.com>
2019-07-03 09:06:03 +0200

    Endret menypunktene slik at Etterregistrer og endre meldeform ikke vises når menyen lastes første gang. Engret også teksten på knappen på etterregistrer meldekort-siden til 'Neste' i stede for 'Til innsending av meldekort'

--- src/app/actions/meldekort.tsx
+++ src/app/actions/meldekort.tsx
@@ -3,13 +3,15 @@ import { AxiosError } from 'axios';
 import { SendtMeldekort } from '../types/meldekort';
 
 export enum MeldekortTypeKeys {
-    API_KALL_FEILET = 'API_KALL_FEILET',
-    LEGG_TIL_INNSENDT_MELDEKORT = 'LEGG_TIL_INNSENDT_MELDEKORT'
+  API_KALL_FEILET = 'API_KALL_FEILET',
+  LEGG_TIL_INNSENDT_MELDEKORT = 'LEGG_TIL_INNSENDT_MELDEKORT',
 }
 
 export const MeldekortActions = {
-    apiKallFeilet: createStandardAction(MeldekortTypeKeys.API_KALL_FEILET)<AxiosError>(),
-    leggTilInnsendtMeldekort: createStandardAction(MeldekortTypeKeys.LEGG_TIL_INNSENDT_MELDEKORT)<SendtMeldekort[]>()
+  apiKallFeilet: createStandardAction(MeldekortTypeKeys.API_KALL_FEILET)<AxiosError>(),
+  leggTilInnsendtMeldekort: createStandardAction(MeldekortTypeKeys.LEGG_TIL_INNSENDT_MELDEKORT)<
+    SendtMeldekort[]
+  >(),
 };
 
 export type MeldekortActionTypes = ActionType<typeof MeldekortActions>;
--- src/app/utils/menyConfig.tsx
+++ src/app/utils/menyConfig.tsx
@@ -7,58 +7,58 @@ import OfteStilteSporsmal from '../sider/ofteStilteSporsmal/ofteStilteSporsmal';
 import { ComponentClass } from 'react';
 
 export interface MenyPunkt {
-    exact?: boolean;
-    component: ComponentClass;
-    urlparam: string;
-    tekstid: string;
-    tittel: string;
-    disabled: boolean;
+  exact?: boolean;
+  component: ComponentClass;
+  urlparam: string;
+  tekstid: string;
+  tittel: string;
+  disabled: boolean;
 }
 
 const menyConfig: MenyPunkt[] = [
-    {
-        exact: true,
-        component: SendMeldekort,
-        tittel: 'sendMeldekort',
-        tekstid: 'naviger.send',
-        urlparam: '/send-meldekort',
-        disabled: false
-    },
-    {
-        component: TidligereMeldekort,
-        tekstid: 'sekundarmeny.tidligere',
-        tittel: 'tidligereMeldekort',
-        urlparam: '/tidligere-meldekort',
-        disabled: false,
-    },
-    {
-        component: EtterregistrerMeldekort,
-        tekstid: 'sekundarmeny.etterregistrer',
-        tittel: 'etterregistrering',
-        urlparam: '/etterregistrer-meldekort',
-        disabled: false,
-    },
-    {
-        component: EndreMeldeform,
-        tekstid: 'sekundarmeny.endreMeldeform',
-        tittel: 'endreMeldeform',
-        urlparam: '/endre-meldeform',
-        disabled: false,
-    },
-    {
-        component: OmMeldekort,
-        tekstid: 'sekundarmeny.omMeldekort',
-        tittel: 'omMeldekort',
-        urlparam: '/om-meldekort',
-        disabled: false,
-    },
-    {
-        component: OfteStilteSporsmal,
-        tekstid: 'sekundarmeny.faq',
-        tittel: 'ofteStilteSporsmal',
-        urlparam: '/ofte-stilte-sporsmal',
-        disabled: false,
-    }
+  {
+    exact: true,
+    component: SendMeldekort,
+    tittel: 'sendMeldekort',
+    tekstid: 'naviger.send',
+    urlparam: '/send-meldekort',
+    disabled: false,
+  },
+  {
+    component: TidligereMeldekort,
+    tekstid: 'sekundarmeny.tidligere',
+    tittel: 'tidligereMeldekort',
+    urlparam: '/tidligere-meldekort',
+    disabled: false,
+  },
+  {
+    component: EtterregistrerMeldekort,
+    tekstid: 'sekundarmeny.etterregistrer',
+    tittel: 'etterregistrering',
+    urlparam: '/etterregistrer-meldekort',
+    disabled: true,
+  },
+  {
+    component: EndreMeldeform,
+    tekstid: 'sekundarmeny.endreMeldeform',
+    tittel: 'endreMeldeform',
+    urlparam: '/endre-meldeform',
+    disabled: true,
+  },
+  {
+    component: OmMeldekort,
+    tekstid: 'sekundarmeny.omMeldekort',
+    tittel: 'omMeldekort',
+    urlparam: '/om-meldekort',
+    disabled: false,
+  },
+  {
+    component: OfteStilteSporsmal,
+    tekstid: 'sekundarmeny.faq',
+    tittel: 'ofteStilteSporsmal',
+    urlparam: '/ofte-stilte-sporsmal',
+    disabled: false,
+  },
 ];
 
 export default menyConfig;
