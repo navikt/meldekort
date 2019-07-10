2f1c20f7006cf319aeee749bb83a300073b154c5
My Thao Nguyen <mythng94@gmail.com>
2019-07-10 13:25:56 +0200

    Revert "FIX: Utskrift i IE"
    This reverts commit 057c0ba116f2d1d86b9a4c8272f3d63dac50829f.

--- src/app/components/print/utskrift.tsx
+++ src/app/components/print/utskrift.tsx
@@ -6,71 +6,67 @@ import { RootState } from '../../store/configureStore';
 import { connect } from 'react-redux';
 import { FormattedMessage } from 'react-intl';
 import { hentIntl } from '../../utils/intlUtil';
-import classNames from 'classnames';
-import { isIE } from '../../utils/browsers';
 
 /**
  * Innhold som legges her er skult på skjerm, men vises på utskrift
  */
 
 export interface Props extends ClassAttributes<any> {
-  active?: boolean;
-  erKvittering?: boolean;
+    active?: boolean;
+    erKvittering?: boolean;
 }
 
 export interface SideProps extends ClassAttributes<any> {
-  tittel?: string;
+    tittel?: string;
 }
 
 export const Side: React.FunctionComponent<SideProps> = ({ children, tittel }) => (
-  <div className="utskrift__side">
-    {tittel && <Sidetittel className="utskrift__sidetittel">{tittel}</Sidetittel>}
-    {children}
-  </div>
+    <div className="utskrift__side">
+        {tittel && <Sidetittel className="utskrift__sidetittel">{tittel}</Sidetittel>}
+        {children}
+    </div>
 );
 
 const mapStateToProps = (state: RootState) => {
-  return {
-    person: state.person,
-    personInfo: state.personInfo.personInfo,
-  };
+    return {
+        person: state.person,
+        personInfo: state.personInfo.personInfo,
+    };
 };
 
 type UtskriftProps = Props & ReturnType<typeof mapStateToProps>;
 
 const Utskrift: React.FunctionComponent<UtskriftProps> = props => {
-  if (!props.active) {
-    return null;
-  }
+    if (!props.active) {
+        return null;
+    }
 
-  const { fornavn, etternavn, fodselsnr } = props.personInfo;
-  const printTekst = hentIntl().formatMessage({ id: 'overskrift.meldekort.sendt' });
-  const stylingMedIE = classNames('utskrift', {
-    ie11: isIE,
-  });
-  return (
-    <div className={stylingMedIE} role="presentation">
-      <div className="utskrift__hode">
-        <img src={NavLogo} alt="" width="90" height="56" />
-        {props.erKvittering ? (
-          <Innholdstittel className="flex-innhold sentrert">{printTekst}</Innholdstittel>
-        ) : (
-          <>
-            <Ingress className="flex-innhold sentrert">
-              <FormattedMessage id={'meldekort.for'} />
-            </Ingress>
-            <Innholdstittel className="flex-innhold sentrert">
-              <span>{`${fornavn} ${etternavn} (${fodselsnr})`}</span>
-            </Innholdstittel>
-          </>
-        )}
-      </div>
-      <div className="utskrift__innhold">{props.children}</div>
-    </div>
-  );
+    const { fornavn, etternavn, fodselsnr } = props.personInfo;
+    const printTekst = hentIntl().formatMessage({ id: 'overskrift.meldekort.sendt' });
+
+    return (
+        <div className="utskrift" role="presentation">
+            <div className="utskrift__hode">
+                <img src={NavLogo} alt="" width="90" height="56" />
+                {props.erKvittering ? (
+                    <Innholdstittel className="flex-innhold sentrert">{printTekst}</Innholdstittel>
+                ) : (
+                    <>
+                        <Ingress className="flex-innhold sentrert">
+                            <FormattedMessage id={'meldekort.for'} />
+                        </Ingress>
+                        <Innholdstittel className="flex-innhold sentrert">
+                            <span>{`${fornavn} ${etternavn} (${fodselsnr})`}</span>
+                        </Innholdstittel>
+                    </>
+                )}
+            </div>
+            <div className="utskrift__innhold">{props.children}</div>
+        </div>
+    );
 };
 
 export default connect(
-  mapStateToProps,
-  null
+    mapStateToProps,
+    null
 )(Utskrift);
--- src/app/utils/browsers.tsx
+++ src/app/utils/browsers.tsx
@@ -3,6 +3,3 @@ const isEdge = window.navigator.userAgent.indexOf('Edge') !== -1;
 
 // Internet Explorer 6-11
 export const isIE = window.navigator.userAgent.indexOf('Trident') !== -1 && !isEdge;
-
-// Firefox
-export const isFireFox = window.navigator.userAgent.indexOf('Mozilla') !== -1;
