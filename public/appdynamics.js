9a6617fd2e4cc6051f6058c1537ef6b243b012f5
Yrjan Fraschetti <yrjanaff@gmail.com>
2019-08-14 11:13:52 +0200

    Prøver å endre hvordan man tester på location i appdynamicsscriptet

--- public/appdynamics.js
+++ public/appdynamics.js
@@ -35,35 +35,39 @@ try {
 }*/
 
 window['adrum-start-time'] = new Date().getTime();
+function isRunningInProd() {
+    return (window.location.href.indexOf("//nav.no") > -1) || (window.location.href.indexOf("//www.nav.no") > -1);
+}
 (function (config) {
     var appKey = "";
-    if (window.location.href.indexOf("tjenester-q1.nav.no") > -1)
-    {             appKey = 'EUM-AAB-AWY';         }
-    else if (window.location.href.indexOf("tjenester.nav.no") > -1)
-    {             appKey = 'EUM-AAB-AWX';         }
+    if (isRunningInProd()) {
+        appKey = 'EUM-AAB-AWX';
+    } else {
+        appKey = 'EUM-AAB-AWY';
+    }
 
-        config.appKey = appKey;
-        if (window.location.href.indexOf("tjenester.nav.no") > -1) {
-            config.beaconUrlHttp = 'http://eumgw.nav.no';
-            config.beaconUrlHttps = 'https://eumgw.nav.no';
-            config.adrumExtUrlHttp = 'http://jsagent.nav.no';
-            config.adrumExtUrlHttps = 'https://jsagent.nav.no';
-        } else {
-            config.beaconUrlHttp = 'http://eumgw.adeo.no';
-            config.beaconUrlHttps = 'https://eumgw.adeo.no';
-            config.adrumExtUrlHttp = 'http://jsagent.adeo.no';
-            config.adrumExtUrlHttps = 'https://jsagent.adeo.no';
-        }
+    config.appKey = appKey;
+    if (isRunningInProd()) {
+        config.beaconUrlHttp = 'http://eumgw.nav.no';
+        config.beaconUrlHttps = 'https://eumgw.nav.no';
+        config.adrumExtUrlHttp = 'http://jsagent.nav.no';
+        config.adrumExtUrlHttps = 'https://jsagent.nav.no';
+    } else {
+        config.beaconUrlHttp = 'http://eumgw.adeo.no';
+        config.beaconUrlHttps = 'https://eumgw.adeo.no';
+        config.adrumExtUrlHttp = 'http://jsagent.adeo.no';
+        config.adrumExtUrlHttps = 'https://jsagent.adeo.no';
+    }
 
-        config.xd = {enable: false};
-        config.spa = {"spa2": true};
+    config.xd = {enable: false};
+    config.spa = {"spa2": true};
 
-    })(window['adrum-config'] || (window['adrum-config'] = {}));
+})(window['adrum-config'] || (window['adrum-config'] = {}));
 
-if (window.location.href.indexOf("tjenester.nav.no") > -1) {
+if (isRunningInProd()) {
     document.write(unescape('%3Cscript') + "src='https://jsagent.nav.no/adrum/adrum.js' " + " type='text/javascript' charset='UTF-8'"  + unescape('%3E%3C/script%3E'));
 } else {
-    if ('https:' === document.location.protocol) {
+    if (document.location.protocol.indexOf('https:') > -1) {
         document.write(unescape('%3Cscript') + " src='https://jsagent.adeo.no/adrum/adrum.js' " + " type='text/javascript' charset='UTF-8'" + unescape('%3E%3C/script%3E'));
     }
     else {
