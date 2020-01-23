49f49b795cb2f2dd921a8633410eb9ef36c67122
gskjeretNAV <50487658+gskjeretNAV@users.noreply.github.com>
2020-01-23 09:17:54 +0100

    La på infotekst på utfyllingssiden. Tekst bør legges i tekstfil dersom dette alternativet velges.

--- src/app/sider/innsending/2-utfyllingsside/utfyllingsside.tsx
+++ src/app/sider/innsending/2-utfyllingsside/utfyllingsside.tsx
@@ -247,6 +247,9 @@ class Utfyllingsside extends React.Component<
           <Sprakvelger />
         </section>
         <section className="seksjon">
+          <div id="ekstrainfo" className="seksjon">
+            Her skal du fylle inn detaljer for det du valgte på forrige side.
+          </div>
           <div id="feilmelding">{this.hentFeilmeldinger()}</div>
           <div className={'utfylling-container'}>
             <UkePanel
