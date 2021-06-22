6a955399103999540b697f030b4725ec84879779
Igor Shuliakov <igor.shuliakov@nav.no>
2021-06-22 11:18:20 +0200

    MELDEPLIKT-280 Nye tekster for mottakere av tiltakspenger, endringer i tekster for mottakere av dagpenger

--- src/app/components/ukepanel/ukepanel.tsx
+++ src/app/components/ukepanel/ukepanel.tsx
@@ -111,7 +111,9 @@ const UkePanel: React.FunctionComponent<UkePanelProps> = props => {
           <Aktivitetsrad
             ukeNummer={props.ukenummer}
             tekstId="utfylling.ferieFravar"
-            forklaringId={'forklaring.utfylling.ferieFravar'}
+            forklaringId={
+              'forklaring.utfylling.ferieFravar' + props.typeYtelsePostfix
+            }
             typeYtelsePostfix={props.typeYtelsePostfix}
             feil={props.utfyllingFeil.feilIFerie.feil}
             feilIDager={props.utfyllingFeil.feilIDagerHorisontal
--- src/app/reducers/aktivtMeldekortReducer.tsx
+++ src/app/reducers/aktivtMeldekortReducer.tsx
@@ -33,7 +33,7 @@ const aktivtMeldekortReducer = (
 ): Meldekort => {
   switch (action.type) {
     case getType(AktivtMeldekortActions.oppdaterAktivtMeldekort):
-      return { ...state, ...action.payload };
+      return { ...state, ...action.payload, meldegruppe: Meldegruppe.INDIV };
 
     case getType(AktivtMeldekortActions.resettAktivtMeldekort):
       return { ...initialState };
