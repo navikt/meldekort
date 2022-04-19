57c273122a16b1d23d679af5983383da8679ceec
Audun Persson <42343764+audun-persson@users.noreply.github.com>
2022-04-19 10:27:28 +0200

    Mp 394 loginservice (#231)
    * Endret til å bruke loginservice fra GCP i dev

--- src/app/utils/env.tsx
+++ src/app/utils/env.tsx
@@ -2,37 +2,37 @@ import { erLocalhost, erMock } from '../mock/utils';
 import { Konstanter } from './consts';
 
 const Environment = () => {
-  if (window.location.hostname.indexOf('www-q0.nav.no') > -1) {
+  if (window.location.hostname.indexOf('www-q0.dev.nav.no') > -1) {
     return {
-      dittNavUrl: 'https://www-q0.nav.no/person/dittnav',
-      apiUrl: 'https://www-q0.nav.no/meldekort/meldekort-api/api/',
-      loginUrl: 'https://loginservice-q.nav.no/login?level=Level3',
-      logoutUrl: 'https://loginservice-q.nav.no/slo',
+      dittNavUrl: 'https://www.dev.nav.no/person/dittnav',
+      apiUrl: 'https://www-q0.dev.nav.no/meldekort/meldekort-api/api/',
+      loginUrl: 'https://loginservice.dev.nav.no/login?level=Level3',
+      logoutUrl: 'https://loginservice.dev.nav.no/slo',
       amplitudeUrl: 'amplitude.nav.no/collect',
       amplitudeKey: '2f190e67f31d7e4719c5ff048ad3d3e6',
     };
-  } else if (window.location.hostname.indexOf('www-q1.nav.no') > -1) {
+  } else if (window.location.hostname.indexOf('www-q1.dev.nav.no') > -1) {
     return {
-      dittNavUrl: 'https://www-q1.nav.no/person/dittnav',
-      apiUrl: 'https://www-q1.nav.no/meldekort/meldekort-api/api/',
-      loginUrl: 'https://loginservice-q.nav.no/login?level=Level3',
-      logoutUrl: 'https://loginservice-q.nav.no/slo',
+      dittNavUrl: 'https://www.dev.nav.no/person/dittnav',
+      apiUrl: 'https://www-q1.dev.nav.no/meldekort/meldekort-api/api/',
+      loginUrl: 'https://loginservice.dev.nav.no/login?level=Level3',
+      logoutUrl: 'https://loginservice.dev.nav.no/slo',
       amplitudeUrl: 'amplitude.nav.no/collect',
       amplitudeKey: '2f190e67f31d7e4719c5ff048ad3d3e6',
     };
   } else if (erMock()) {
     return {
-      dittNavUrl: 'https://www.nav.no/person/dittnav',
+      dittNavUrl: 'https://www.dev.nav.no/person/dittnav',
       apiUrl: '',
-      loginUrl: 'https://loginservice-q.nav.no/login?level=Level3',
-      logoutUrl: 'https://loginservice-q.nav.no/slo',
+      loginUrl: 'https://loginservice.dev.nav.no/login?level=Level3',
+      logoutUrl: 'https://loginservice.dev.nav.no/slo',
     };
   } else if (erLocalhost()) {
     return {
-      dittNavUrl: 'https://www.nav.no/person/dittnav',
+      dittNavUrl: 'https://www.dev.nav.no/person/dittnav',
       apiUrl: 'http://localhost:8801/meldekort/meldekort-api/api/',
-      loginUrl: 'https://loginservice-q.nav.no/login?level=Level3',
-      logoutUrl: 'https://loginservice-q.nav.no/slo',
+      loginUrl: 'https://loginservice.dev.nav.no/login?level=Level3',
+      logoutUrl: 'https://loginservice.dev.nav.no/slo',
     };
   }
   return {
