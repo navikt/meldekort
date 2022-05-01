d49c1ac1caafaff39d289f339100c6acb2396965
Igor Shuliakov <igor.shuliakov@nav.no>
2022-05-01 02:31:43 +0200

    MELDPELIKT-306 Node 17 fix

--- src/__test__/components/utskrift.snapshot.test.tsx
+++ src/__test__/components/utskrift.snapshot.test.tsx
@@ -2,11 +2,28 @@ import * as React from 'react';
 import * as renderer from 'react-test-renderer';
 import ProviderWrapper from '../testSetup/providerWrapper';
 import Utskrift from '../../app/components/print/utskrift';
+import { MeldeForm, Person, PersonInfo } from '../../app/types/person';
 
+const person: Person = {
+  maalformkode: '',
+  meldeform: MeldeForm.ELEKTRONISK,
+  meldekort: [],
+  etterregistrerteMeldekort: [],
+  fravaer: [],
+  id: '',
+  antallGjenstaaendeFeriedager: 0,
+};
+
+const personInfo: PersonInfo = {
+  personId: 0,
+  fodselsnr: '',
+  etternavn: '',
+  fornavn: '',
+};
 it('Utskrift', () => {
   const tree = renderer.create(
     <ProviderWrapper>
-      <Utskrift />
+      <Utskrift person={person} personInfo={personInfo} />
     </ProviderWrapper>
   );
   expect(tree).toMatchSnapshot();
