92af615bfabdbb263acd3e4133362a00cfa6f660
Hanne Olsen <hanne.eilertsen.olsen@nav.no>
2023-07-07 09:10:09 +0200

    Første utkast på oppsett av express-server og endret dockerfile (#417)
    * Første utkast på oppsett av express-server og endret dockerfile
    
    * Fjerner ubrukt task
    
    * Fjerner ekstra mellomrom
    
    * oppretter object hvis window.mwlswkort er undefined
    
    * node 16 i Dockerfile
    
    * Fjernet NAV_DEKORATOREN_URL
    
    ---------
    
    Co-authored-by: Igor Shuliakov <igor.shuliakov@nav.no>

---
+++ server/logger.js
@@ -0,0 +1,20 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  format:
+    process.env.NODE_ENV === 'development'
+      ? winston.format.simple()
+      : winston.format.json(),
+  transports: new winston.transports.Console(),
+});
+
+const logRequests = (request, res, next) => {
+  const method = request.method;
+  const url = request.url;
+
+  logger.info(`${method} ${url}`);
+
+  next();
+};
+
+module.exports = { logger, logRequests };
---
+++ server/server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const compression = require('compression');
+const {
+  injectDecoratorServerSide,
+} = require('@navikt/nav-dekoratoren-moduler/ssr');
+const { logger, logRequests } = require('./logger');
+const path = require('path');
+
+const port = process.env.PORT || 8080;
+const basePath = '/meldekort';
+const buildPath = path.resolve(__dirname, '../build');
+
+const app = express();
+
+// Gzip responser
+app.use(compression());
+
+// Sikkerhetsgreier
+app.disable('x-powered-by');
+
+// Cache public-filer (som favicon) i én time
+app.use(`${basePath}`, express.static('public', { maxAge: '1h' }));
+
+console.log(`Dekoratormiljø: ${process.env.DEKORATOR_MILJO}`);
+
+app.use(basePath, express.static(buildPath, { index: false }));
+
+app.get(`${basePath}/internal/isAlive|isReady`, (_, res) =>
+  res.sendStatus(200)
+);
+
+app.use(/^(?!.*\/(internal|static)\/).*$/, (req, res) => {
+  injectDecoratorServerSide({
+    env: process.env.DEKORATOR_MILJO ?? 'dev',
+    filePath: `${buildPath}/index.html`,
+    simple: true,
+    feedback: false,
+    chatbot: false,
+    shareScreen: false,
+  })
+    .then(text => res.send(text))
+    .catch(e => {
+      console.error(`Failed to get decorator: ${e}`);
+      res
+        .status(500)
+        .send('Det har oppstått en feil. Venligst prøv igjen senere.');
+    });
+});
+
+app.use(logRequests);
+
+app.listen(port, () => {
+  logger.info(`Server kjører på port ${port}`);
+});
