de811f915242dca2d542db3b75b8960312da6258
Igor Weber <igor.shuliakov@nav.no>
2024-03-13 10:08:27 +0100

    Korrigert_elektronisk og ferdi får fargen grønn

--- src/app/utils/statusEtikettUtil.tsx
+++ src/app/utils/statusEtikettUtil.tsx
@@ -1,7 +1,7 @@
 import { KortStatus, KortType } from "../types/meldekort";
 
 export const finnRiktigTagVariant = (status: KortStatus, kortType: KortType): "success" | "info" | "warning" | "error" | "alt3" => {
-  if (kortType === KortType.KORRIGERT_ELEKTRONISK) return "alt3";
+  if (kortType === KortType.KORRIGERT_ELEKTRONISK && status === KortStatus.IKKE) return "alt3";
 
   switch (status) {
     case KortStatus.KLAR:
